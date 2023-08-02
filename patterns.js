e7da0efa8085751128c0f193f81738f17297e1fe
Praveen-2022 <praveenkumarchauhan2022@gmail.com>
2023-08-02 15:03:10 +0530

    astrikChangeToNumber

--- patterns.js
+++ patterns.js
@@ -13,7 +13,6 @@ function simplePattern() {
     }
     console.log(pattern);
   }
-
 }
 // simplePattern();
 
@@ -36,43 +35,86 @@ function halfPyramid(nums = 5) {
 
 // halfPyramid();
 
-
 // * * * * *
 // *       *
 // *       *
 // * * * * *
 
-function hollowPyramid(){
-    let n=4;
-    let m=5;
-    for(let i=1;i<=4;i++){
-        let pattern ="";
-        for(let j=1;j<=5;j++){
-            if(i === 1 || j === 1 || i === n || j === m){
-                pattern += "* ";
-            }else{
-                pattern +="  " //dual space
-            }
-        }
-        console.log(pattern);
+function hollowPyramid() {
+  let n = 4;
+  let m = 5;
+  for (let i = 1; i <= 4; i++) {
+    let pattern = "";
+    for (let j = 1; j <= 5; j++) {
+      if (i === 1 || j === 1 || i === n || j === m) {
+        pattern += "* ";
+      } else {
+        pattern += "  "; //dual space
+      }
     }
+    console.log(pattern);
+  }
 }
 
-hollowPyramid();
+// hollowPyramid();
 
+// Inverted Half Pyramid
+// * * * * *
+// * * * *
+// * * *
+// * *
+// *
 
+function inverted(n = 5) {
+  for (let i = 1; i <= n; i++) {
+    let pattern = "";
+    for (let j = 1; j <= n - i + 1; j++) {
+      pattern += "* ";
+    }
+    console.log(pattern);
+  }
+}
 
+// inverted();
 
+// Half Pyramid Rotated  180 Degree
 
+//         *
+//       * *
+//     * * *
+//   * * * *
+// * * * * *
+function rotated(n) {
+  for (let i = 1; i <= n; i++) {
+    let pattern = "";
+    for (let j = 1; j <= n - i; j++) {
+      pattern += " ";
+    }
+    for (let k = 1; k <= i; k++) {
+      pattern += "*";
+    }
+    console.log(pattern);
+  }
+}
 
+// rotated(5);
 
+// 1
+// 1 2
+// 1 2 3
+// 1 2 3 4
+// 1 2 3 4 5
 
+function astrikChangeToNumber() {
+  let n = 5;
+  let number = 1;
+  for (let i = 1; i <= n; i++) {
+    let pattern = "";
+    for (let j = 1; j <= i; j++) {
+      pattern += j + " ";
+    }
+    console.log(pattern);
+  }
+}
 
-
-
-
-
-
-
-
-
+// astrikChangeToNumber();
