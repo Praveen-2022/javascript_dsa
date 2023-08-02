2a7800e088ef93d80b089d31fd1e4d8b11411d7e
Praveen-2022 <praveenkumarchauhan2022@gmail.com>
2023-08-02 11:13:00 +0530

    hollow pattern print

--- patterns.js
+++ patterns.js
@@ -1,3 +1,22 @@
+// *****
+// *****
+// *****
+// *****
+
+function simplePattern() {
+  let n = 4;
+  let m = 5;
+  for (let i = 1; i <= n; i++) {
+    let pattern = "";
+    for (let j = 1; j <= m; j++) {
+      pattern += "* ";
+    }
+    console.log(pattern);
+  }
+
+}
+// simplePattern();
+
 // Half Pyramind Patterns
 // *
 // **
@@ -5,14 +24,55 @@
 // ****
 // *****
 
-function halfPyramid(nums =5){
-    for(let i=1;i<=nums;i++){
-        let pattern="";
-        for(let j=1;j<=i;j++){
-            pattern +="* "
+function halfPyramid(nums = 5) {
+  for (let i = 1; i <= nums; i++) {
+    let pattern = "";
+    for (let j = 1; j <= i; j++) {
+      pattern += "* ";
+    }
+    console.log(pattern);
+  }
+}
+
+// halfPyramid();
+
+
+// * * * * *
+// *       *
+// *       *
+// * * * * *
+
+function hollowPyramid(){
+    let n=4;
+    let m=5;
+    for(let i=1;i<=4;i++){
+        let pattern ="";
+        for(let j=1;j<=5;j++){
+            if(i === 1 || j === 1 || i === n || j === m){
+                pattern += "* ";
+            }else{
+                pattern +="  " //dual space
+            }
         }
-        console.log(pattern)
+        console.log(pattern);
     }
 }
 
-halfPyramid();
+hollowPyramid();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
