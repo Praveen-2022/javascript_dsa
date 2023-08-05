34f49d5b8ce204fe2ec1e4232deda69b6da048cb
Praveen-2022 <praveenkumarchauhan2022@gmail.com>
2023-08-05 14:38:04 +0530

    butterfly and rrombus pattern done

--- advance_patterns.js
+++ advance_patterns.js
@@ -43,3 +43,20 @@ function butterFly() {
 
 // butterFly();
 
+// Rhombus Pattern
+
+function rombus() {
+  let n = 5;
+  for (let i =1;i<=n;i++) {
+    let pattern = "";
+    for (let j = 1; j <=n-i; j++) {
+      pattern += " ";
+    }
+    for(let j=1;j<=5;j++){
+        pattern +="* "
+    }
+    console.log(pattern);
+  }
+}
+
+rombus();
