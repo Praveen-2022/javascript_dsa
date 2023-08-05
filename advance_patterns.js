5b4b54c035e08c8f7c6551e5a29f39b2875db7b1
Praveen-2022 <praveenkumarchauhan2022@gmail.com>
2023-08-05 14:37:01 +0530

    solving advance patterns

--- advance_patterns.js
+++ advance_patterns.js
@@ -1,1 +1,45 @@
-console.log("hello Praveen");
+// ButterFly Pattern
+
+function butterFly() {
+  let n = 5;
+
+  //for upper Half
+  for (let i = 1; i <= n; i++) {
+    let pattern = "";
+    for (let j = 1; j <= i; j++) {
+      pattern += "*" + " ";
+    }
+
+    //for spaces
+    let spaces = 2 * (n - i);
+    for (let j = 1; j <= spaces; j++) {
+      pattern += "  ";
+    }
+    for (let j = 1; j <= i; j++) {
+      pattern += "*" + " ";
+    }
+    console.log(pattern);
+  }
+
+  // for lower half
+
+  for (let i = n; i >= 1; i--) {
+    let pattern = "";
+    for (let j = 1; j <= i; j++) {
+      pattern += "*" + " ";
+    }
+
+    //for spaces
+    let spaces = 2 * (n - i);
+    for (let j = 1; j <= spaces; j++) {
+      pattern += "  ";
+    }
+    for (let j = 1; j <= i; j++) {
+      pattern += "*" + " ";
+    }
+    console.log(pattern);
+  }
+}
+
+// butterFly();
+
