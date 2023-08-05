c1ed03b025203f61be51a745f21eb5c93ac578f7
Praveen-2022 <praveenkumarchauhan2022@gmail.com>
2023-08-05 16:05:31 +0530

    adding some new questions

--- advance_patterns.js
+++ advance_patterns.js
@@ -47,16 +47,77 @@ function butterFly() {
 
 function rombus() {
   let n = 5;
-  for (let i =1;i<=n;i++) {
+  for (let i = 1; i <= n; i++) {
     let pattern = "";
-    for (let j = 1; j <=n-i; j++) {
+    for (let j = 1; j <= n - i; j++) {
       pattern += " ";
     }
-    for(let j=1;j<=5;j++){
-        pattern +="* "
+    for (let j = 1; j <= 5; j++) {
+      pattern += "* ";
     }
     console.log(pattern);
   }
 }
 
-rombus();
+// rombus();
+
+//number Pyramid
+
+//     1
+//    2 2
+//   3 3 3
+//  4 4 4 4
+// 5 5 5 5 5
+
+function pyramidNum() {
+  let n = 5;
+  for (let i = 1; i <= n; i++) {
+    let pattern = "";
+    //for spaces
+
+    for (let j = 1; j <= n - i; j++) {
+      pattern += " ";
+    }
+    // for number
+
+    for (let j = 1; j <= i; j++) {
+      pattern += i + " ";
+    }
+    console.log(pattern);
+  }
+}
+// pyramidNum();
+
+// Palindromic Number pattern
+//         1
+//       2 1 2
+//     3 2 1 2 3
+//   4 3 2 1 2 3 4
+// 5 4 3 2 1 2 3 4 5
+
+function PalindromicNum() {
+  let n = 5;
+  for (let i = 1; i <= n; i++) {
+    let pattern = "";
+    // for spaces
+    for (let j = 1; j <= n - i; j++) {
+      pattern +="  "
+    }
+ //for first half number
+
+// tricky part  
+ for(let j=i;j>=1;j--){
+  pattern += j + " ";
+ }
+
+ // for another half 
+  for (let j = 2; j <= i; j++) {
+    pattern += j + " ";
+  }
+ 
+
+    console.log(pattern);
+  }
+}
+
+PalindromicNum();
